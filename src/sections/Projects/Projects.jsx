b676751d5f90af6dc9690e317508d25b8874345a
Aidelle <agentneme@gmail.com>
2024-06-23 19:55:59 +0800

    Finish Project

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './ProjectsStyles.module.css';
+import lumawig from '../../assets/lumawig.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+        <h1 className="sectionTitle">Projects</h1>
+        <div className={styles.projectsContainer}>
+           
+            <ProjectCard src={lumawig} 
+            link=""
+            h3="lumawig"
+            p="Browser Extension"
+            
+            />
+        </div>
+    </section>
+  );
+}
+
+export default Projects
